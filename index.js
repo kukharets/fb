3055725fba289cde0794d2e3d91bd3f3f43690ab
Taras Kukharets <taras.kukharets@gmail.com>
2016-09-01 12:31:17 +0000

    Fibonacci homework

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getNum() {
 }
 
 
-function fibcicle(userNum) {
+var fibcicle = function (userNum) {
     var firctCicleNum = 0;
     var secondCicleNum = 1;
     var tempCicleNum;
@@ -23,20 +23,23 @@ function fibcicle(userNum) {
 }
 
 
-var fibStartCount = 3;
-var tempRecursionNum;
-var firstRecursionNum = 0;
-var secondRecursionNum = 1;
-function fibrec(userNum) {
-
-    tempRecursionNum = firstRecursionNum + secondRecursionNum;
-    firstRecursionNum = secondRecursionNum;
-    secondRecursionNum = tempRecursionNum;
-    fibStartCount++;
-    if (fibStartCount <= userNum) {
-        fibrec(userNum);
+var fibrec = function (userNum){
+    var fibStartCount = 3;
+    var tempRecursionNum;
+    var firstRecursionNum = 0;
+    var secondRecursionNum = 1;
+    function _fibrec(userNum) {
+    
+        tempRecursionNum = firstRecursionNum + secondRecursionNum;
+        firstRecursionNum = secondRecursionNum;
+        secondRecursionNum = tempRecursionNum;
+        fibStartCount++;
+        if (fibStartCount <= userNum) {
+            _fibrec(userNum);
+        }
+    
+        document.getElementById("recursionResult").innerHTML = "Recursion res: " +
+            tempRecursionNum;
     }
-
-    document.getElementById("recursionResult").innerHTML = "Recursion res: " +
-        tempRecursionNum;
+    _fibrec(userNum);
 }
